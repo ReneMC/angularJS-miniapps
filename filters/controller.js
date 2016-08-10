e9c1bfee3beaf0d61fd3e1e5c858503b0df03cef
renem <rene.mejiac@gmail.com>
2016-08-10 15:52:24 -0700

    + Filters

---
+++ filters/controller.js
@@ -0,0 +1,27 @@
+var app = angular.module("filtersModule",[])
+	.filter("removeHtml",function(){
+		return function(texto){
+			return String(texto).replace(/<[^>]+>/gm, '');
+		}
+	})
+	app.controller("FiltersController",function($scope,$http){
+		$scope.mi_html = '\n<h1 class="super-gigante">Hola!</h1>\n<p class="useless-paragraph">Esto es un parrafo!</>'
+		$scope.unit = 100;
+		$scope.moneda = {};
+		
+		// To calculate exchange rate directly from bitapeso.com's API
+		$http.get('http://bitapeso.com/json/')
+		.success(function(data){
+			$scope.moneda = data;
+		})
+		// In case Cross Origin issue appears we hard code the object's values
+		.error(function(err){
+			var data = {
+				"btc": 1, 
+				"dolar": 18.400999, 
+				"mxn": 10998.01 ,
+				"usd": 598
+			}
+			$scope.moneda = data;
+		});
+	});
