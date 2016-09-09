ddeb49fc1d34c206e3923547f5f72a374959d689
renem <rene.mejiac@gmail.com>
2016-09-08 18:33:15 -0700

    + Factories

---
+++ factories/controller.js
@@ -0,0 +1,57 @@
+angular.module("ToDoList", ["LocalStorageModule"])
+.factory('ToDoService', function(localStorageService){
+	var toDoService = {};
+	
+	toDoService.key = "angular-todolist";
+	
+	if(localStorageService.get(toDoService.key)){
+		toDoService.activities = localStorageService.get(toDoService.key);
+	}
+	else{
+		toDoService.activities = [];
+	}
+	
+	toDoService.add = function(newActv){
+		toDoService.activities.push(newActv);
+		toDoService.updaLocalStorage();
+	};
+	toDoService.updaLocalStorage = function(){
+		localStorageService.set(toDoService.key,toDoService.activities);
+	};
+	toDoService.clean = function(){
+		toDoService.activities = [];
+		toDoService.updaLocalStorage();
+	};
+	toDoService.getAll = function(){
+		return toDoService.activities;
+	};
+	toDoService.removeItem = function(item){
+		toDoService.activities = toDoService.activities.filter(function(activity){
+			return activity !== item;
+		});
+		toDoService.updaLocalStorage();
+		return toDoService.getAll();
+		/*
+			[{actividad: 'terminar el curso', fecha: ' '},{},{},{}] -> toDoService.activities
+			Conserva todos los registros que sean diferentes a item
+		*/
+	};
+	
+	return toDoService;
+})
+.controller("ToDoController", function($scope,ToDoService){
+	
+	$scope.todo = ToDoService.getAll();
+	$scope.newActv = {};
+	
+	$scope.addActv = function(){
+		ToDoService.add($scope.newActv);
+		$scope.newActv = {};
+	}
+	$scope.removeActv = function(item){
+		$scope.todo = ToDoService.removeItem(item);
+	}
+	$scope.clean = function(){
+		ToDoService.clean();
+	}
+});
